53c2a133d1763f868200e2cc0289837cb2505928
eslam <eslammongy995@gmail.com>
2021-07-29 13:58:19 +0200

    connect with firebase

--- app/src/main/java/com/eslammongy/quizeapp/MainActivity.kt
+++ app/src/main/java/com/eslammongy/quizeapp/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.eslammongy.quizeapp
 
+import android.app.Application
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.FirebaseApp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
--- app/src/main/java/com/eslammongy/quizeapp/SplashFragment.kt
+++ app/src/main/java/com/eslammongy/quizeapp/SplashFragment.kt
@@ -1,59 +1,63 @@
 package com.eslammongy.quizeapp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.eslammongy.quizeapp.databinding.FragmentSplashBinding
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SplashFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SplashFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private var _binding: FragmentSplashBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var navController:NavController
+    private lateinit var firebaseAuth:FirebaseAuth
+    private val START_TAG = "START_LOG"
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+    ): View {
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        FirebaseApp.initializeApp(requireContext())
+        navController = Navigation.findNavController(view)
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.startFeedback.text = ("Checking User Account... ")
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SplashFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SplashFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onStart() {
+        super.onStart()
+
+        val currentUser = firebaseAuth.currentUser
+        if (currentUser == null){
+            binding.startFeedback.text = ("Creating User Account... ")
+            firebaseAuth.signInAnonymously().addOnCompleteListener {
+                if (it.isSuccessful){
+                    binding.startFeedback.text = ("Account Created.. ")
+                    navController.navigate(R.id.action_startFragment_to_listFragment)
+                }else {
+                    Log.d(START_TAG, "Start Log : " + it.exception!!.message)
                 }
             }
+        }else{
+
+            binding.startFeedback.text = ("Login Successfully... ")
+            navController.navigate(R.id.action_startFragment_to_listFragment)
+        }
+
     }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
+
 }
